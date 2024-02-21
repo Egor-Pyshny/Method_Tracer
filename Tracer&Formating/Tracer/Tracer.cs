43c6b2538624bdca5a6f8c23684b9939e029b397
Egor251 <egorpyshny@gmail.com>
2024-02-21 15:02:47 +0300

    add Tracer

---
+++ Tracer&Formating/Tracer/Tracer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tracer_Formating.Tracer
+{
+    public class Tracer : ITracer
+    {
+        private ConcurrentDictionary<int, ConcurrentStack<MethodNode>> stacks;
+
+        public Tracer() => stacks = [];
+
+        public TracerResult GetTraceResult()
+        {
+            TracerResult res = new TracerResult();
+            foreach (KeyValuePair<int, ConcurrentStack<MethodNode>> pair in stacks) {
+                MethodNode parent = new MethodNode();
+                MethodNode test = new MethodNode();
+                bool change_parent = false;
+                var reverseStack1 = new ConcurrentStack<MethodNode>(pair.Value);
+                foreach (MethodNode node in reverseStack1)
+                {
+                    if (!node.isSystem)
+                    {
+                        parent.methods.Add(node);
+                        if (change_parent)
+                        {
+                            node.parent = parent;
+                            parent = node;
+                        }
+                    }
+                    else { 
+                        if (node.__sysName == "start")
+                        {
+                            change_parent = true;
+                        }
+                        else
+                        {
+                            parent = parent.parent;
+                        }
+                    }
+                }
+                ThreadNode th = new ThreadNode();
+                th.tid = pair.Key;
+                long duration = 0;
+                th.methods = parent.methods;
+                th.methods.ForEach(m => duration += m.duration);
+                th.duration = duration;               
+                res._threadNodes.Add(th);
+            }
+
+            return res;
+        }
+
+        public void StartTrace()
+        {
+            var tid = Thread.CurrentThread.ManagedThreadId;
+            if (!stacks.ContainsKey(tid))
+            {
+                stacks[tid] = new ConcurrentStack<MethodNode>();
+            }
+            StackTrace stackTrace = new StackTrace(1);
+            StackFrame? stackFrame = stackTrace.GetFrame(0);
+            if (stackFrame == null)
+            {
+                throw new NullReferenceException("Method stack trace is null");
+            }
+            MethodBase? mb = stackFrame.GetMethod();
+            if (mb == null)
+            {
+                throw new NullReferenceException("Method stack trace is null");
+            }
+            string _class = (mb.ReflectedType == null || mb.ReflectedType.FullName == null) ? "Unknown" : mb.ReflectedType.FullName;
+            MethodNode methodNode = new MethodNode(mb.Name, _class);          
+            MethodNode marker = new MethodNode(null, null, true, "start");
+            stacks[tid].Push(marker);
+            stacks[tid].Push(methodNode);
+            methodNode.stopwatch!.Start();
+        }
+
+        public void StopTrace()
+        {
+            var tid = Thread.CurrentThread.ManagedThreadId;
+            if (!stacks.ContainsKey(tid))
+            {
+                stacks[tid] = new ConcurrentStack<MethodNode>();
+            }
+            StackTrace stackTrace = new StackTrace(1);
+            StackFrame? stackFrame = stackTrace.GetFrame(0);
+            if (stackFrame == null)
+            {
+                throw new NullReferenceException("Method stack trace is null");
+            }
+            MethodBase? mb = stackFrame.GetMethod();
+            if (mb == null)
+            {
+                throw new NullReferenceException("Method stack trace is null");
+            }
+            MethodNode? methodNode = null;
+            MethodNode marker = new MethodNode(null, null, true, "stop");
+            foreach(MethodNode m in stacks[tid]) {
+                if (!m.isSystem && m.closed == false) { 
+                    m.closed = true;
+                    methodNode = m;
+                    break;
+                }
+            }
+            if (methodNode == null) { 
+                throw new NullReferenceException("MethodNode is null, you have at least one extra Tracer.Clsoe()");
+            }
+            methodNode!.stopwatch!.Stop();
+            methodNode.duration = methodNode.stopwatch.ElapsedMilliseconds;
+            stacks[tid].Push(marker);
+        }
+    }
+}
---
+++ Tracer&Formating/Tracer/TracerResult.cs
@@ -0,0 +1,122 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.ServiceModel;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace Tracer_Formating.Tracer
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class TracerResult : IXmlSerializable
+    {
+        [JsonProperty("Threads")]
+        internal List<ThreadNode> _threadNodes;
+        
+        public IReadOnlyList<ThreadNode> threadNodes => _threadNodes;
+
+        public TracerResult() => _threadNodes = [];
+
+        public TracerResult(List<ThreadNode> thNodes) => _threadNodes = thNodes;
+
+        public XmlSchema? GetSchema() { return null; }
+        public void ReadXml(XmlReader reader) { }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            foreach (var thread in threadNodes)
+            {
+                thread.WriteXml(writer);
+            }
+        }
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class MethodNode : IXmlSerializable
+    {
+        [JsonProperty("Method_name", Order = 0)]
+        public string? _name;
+
+        internal string? __sysName;
+
+        [JsonProperty("Method_duration", Order = 2)]
+        public long duration;
+
+        [JsonProperty("Method_class", Order = 1)]
+        public string? _class;
+
+        [XmlIgnore]
+        internal bool isSystem;
+
+        [XmlIgnore]
+        internal bool closed = false;
+
+        [XmlIgnore]
+        internal MethodNode? parent;
+
+        [XmlIgnore]
+        internal Stopwatch? stopwatch;
+
+        [JsonProperty("Nested_methods", Order = 3)]
+        public List<MethodNode> methods;
+
+        public MethodNode() => methods = []; 
+
+        public MethodNode(string? _name, string? _class, bool isSystem = false, string? sysName = null) {
+            this._name = _name; 
+            this._class = _class;
+            this.stopwatch = new Stopwatch();
+            this.isSystem = isSystem;
+            this.__sysName = sysName;      
+            methods = [];
+        }
+
+        public XmlSchema? GetSchema() { return null; }
+        public void ReadXml(XmlReader reader) { }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteStartElement("MethodNode");
+            writer.WriteAttributeString("Method_name", _name!.ToString());
+            writer.WriteAttributeString("Method_duration", duration.ToString());
+            writer.WriteAttributeString("Method_class", _class!.ToString());
+            foreach (var method in methods)
+            {
+                method.WriteXml(writer);
+            }
+            writer.WriteEndElement();
+        }
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class ThreadNode : IXmlSerializable
+    {
+        [JsonProperty("Thread_id")]
+        public int tid;
+
+        [JsonProperty("Thread_duration")]
+        public long duration;
+
+        [JsonProperty("Nested_methods")]
+        public List<MethodNode> methods;
+
+        public ThreadNode() => methods = [];
+
+        public XmlSchema? GetSchema() { return null; }
+        public void ReadXml(XmlReader reader) { }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteStartElement("ThreadNode");
+            writer.WriteAttributeString("Thread_id", tid.ToString());
+            writer.WriteAttributeString("Thread_duration", duration.ToString());
+            foreach (var method in methods) {
+                method.WriteXml(writer);
+            }
+            writer.WriteEndElement();
+        }
+    }
+}
